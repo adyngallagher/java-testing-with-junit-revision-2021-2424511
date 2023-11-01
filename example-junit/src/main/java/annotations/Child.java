f414a3d38f3d8152fb3b08d2bd99f007db628521
Adyn Gallagher <adyn.r.gallagher@gmail.com>
2023-11-01 17:04:14 -0400

    completed 03_04

--- example-junit/src/main/java/annotations/Child.java
+++ example-junit/src/main/java/annotations/Child.java
@@ -1,6 +1,7 @@
 package annotations;
 
 public class Child extends Parent {
+    @Override
     public void someMethod() {
         System.out.println("Overriding method from the parent class");
     }
