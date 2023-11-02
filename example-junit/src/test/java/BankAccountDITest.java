0dfb7c4e4b567ae6b57110fa42fafe09056f2098
Adyn Gallagher <adyn.r.gallagher@gmail.com>
2023-11-02 15:35:57 -0400

    completed ch 3

--- example-junit/src/test/java/BankAccountDITest.java
+++ example-junit/src/test/java/BankAccountDITest.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("Test BankAccount class")
-public class BankAccountTest {
-
+@ExtendWith(BankAccountParameterResolver.class)
+public class BankAccountDITest {
     @Test
     @DisplayName("Withdraw 500 successfully")
     public void testWithdraw() {
@@ -16,9 +16,8 @@ public class BankAccountTest {
 
     @Test
     @DisplayName("Deposit 400 successfully")
-    public void testDeposit() {
-        BankAccount bankAccount = new BankAccount(400, 0);
+    public void testDeposit(BankAccount bankAccount) {
         bankAccount.deposit(500);
-        assertEquals(900, bankAccount.getBalance(), "unexpected value, expected value 900");
+        assertEquals(500, bankAccount.getBalance());
     }
 }
